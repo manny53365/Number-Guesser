4dcb1194e6edc7c14f7e115739cf2eac09523e0d
Manny53365 <mannyrodriguez011@gmail.com>
2021-02-11 00:05:02 -0500

    added the first 2 functions to the script.js file

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+    let random = Math.floor(Math.random(9))
+    return random
+}
 
+function compareGuesses(humanGuess,CPUGuess,generateTarget) {
+    if (humanGuess && CPUGuess == generateTarget) {
+        console.log('It\'s a tie, the human wins.')
+        humanScore += 1
+    } else if (humanGuess > generateTarget && CPUGuess < generateTarget) {
+        console.log('The CPU wins')
+        computerScore += 1
+    } else if (humanGuess < generateTarget && CPUGuess > generateTarget) {
+        console.log('The human wins')
+        humanScore += 1
+    } else {
+        console.log('Invalid input')
+    }
+}
