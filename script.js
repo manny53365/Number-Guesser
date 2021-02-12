ef1edc543e96968106d0053106b5c35f38f2300b
Manny53365 <mannyrodriguez011@gmail.com>
2021-02-11 20:23:17 -0500

    completed project

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-function generateTarget() {
-    let random = Math.floor(Math.random(9))
+let generateTarget = () => {
+    let random = Math.floor(Math.random() * 9)
     return random
 }
 
-function compareGuesses(humanGuess,CPUGuess,generateTarget) {
+let compareGuesses = (humanGuess,CPUGuess,generateTarget) => {
     if (humanGuess && CPUGuess == generateTarget) {
-        console.log('It\'s a tie, the human wins.')
-        humanScore += 1
+        //console.log('It\'s a tie, the human wins.')
+        return true
     } else if (humanGuess > generateTarget && CPUGuess < generateTarget) {
-        console.log('The CPU wins')
-        computerScore += 1
-    } else if (humanGuess < generateTarget && CPUGuess > generateTarget) {
-        console.log('The human wins')
-        humanScore += 1
+        //console.log('The CPU wins')
+        return false
     } else {
-        console.log('Invalid input')
+        //console.log('The human wins')
+        return true
     }
+}
+
+let updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore +=1
+    } else {
+        computerScore +=1
+    }
+}
+
+let advanceRound = () => {
+    currentRoundNumber +=1
 }
